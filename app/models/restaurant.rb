74f8cfcb38c8f0b5078e8bce145bead0d10a580e
Genevieve Warthon Landmann <genevievewarthon@gmail.com>
2023-02-22 12:00:38 +0100

    validation works

---
+++ app/models/restaurant.rb
@@ -0,0 +1,10 @@
+CATEGORY = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true
+  validates :category, inclusion: { in: CATEGORY }
+end
